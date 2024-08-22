359bfb665888632501281aa7f33ede671091e40f
simon-milleto <contact@simonmilleto.com>
2024-08-22 11:55:40 +0200

    fix: build

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import '@radix-ui/themes/styles.css';
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { Theme, ThemePanel } from '@radix-ui/themes';
+import { Theme } from '@radix-ui/themes';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
 
 
-import './index.scss'
+import './index.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
